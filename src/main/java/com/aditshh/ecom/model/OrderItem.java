a2d9fba57f0d23fedc8ad86c717cc75bd91debc7
AditShh28 <ak2057338@gmail.com>
2025-02-10 09:25:29 +0530

    Create entities for model

--- src/main/java/com/aditshh/ecom/model/OrderItem.java
+++ src/main/java/com/aditshh/ecom/model/OrderItem.java
@@ -4,23 +4,21 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
+import java.math.BigDecimal;
 @Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class Comment {
+public class OrderItem {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private String content;
-    private Integer score;
-
+    @ManyToOne
+    @JoinColumn(name="order_id", nullable = false)
+    private Order order;
     @ManyToOne
     @JoinColumn(name="product_id", nullable = false)
     private Product product;
-
-    @ManyToOne
-    @JoinColumn(name="user_id", nullable = false)
-    private User user;
+    private Integer quantity;
+    private BigDecimal price;
 }
